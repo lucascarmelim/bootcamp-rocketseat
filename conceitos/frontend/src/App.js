1f74858cdabf6404b3dee6e9c70ad83e33967f13
lucascarmelim <lucasdcarmelim@gmail.com>
2020-12-29 17:35:06 -0300

    Curso

--- conceitos/frontend/src/App.js
+++ conceitos/frontend/src/App.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import './App.css'
+import backgroundImage from './assets/background.jpg'
 
 import Header from './components/Header';
 
+/**
+ * Componente
+ * Propriedade
+ * Estado
+ */
+
 function App(){
+
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  //useState retorna um array com duas posições
+  //
+  // 1ª Posição - Retorna a variável com o valor inicial
+  // 2ª Posição - Retorna uma função para atualizar o valor da variável contida na posição 1
+
+  function handleAddProject() {
+
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+  }
+
   return (
     <>
-      <Header />
-      <Header />
+      <Header title="Projects"/>
+
+      <img src={backgroundImage}></img>
+
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
     </>
   );
 }
