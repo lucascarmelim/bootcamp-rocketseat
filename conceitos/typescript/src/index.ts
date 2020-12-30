48054614c90a615397528a8e65c96c1c1356fca6
lucascarmelim <lucasdcarmelim@gmail.com>
2020-12-30 15:13:45 -0300

    typescript

--- conceitos/typescript/src/index.ts
+++ conceitos/typescript/src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import { helloWorld } from './route'
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello World'});
-})
+// app.get('/', (request, response) => {
+//   return response.json({message: 'Hello World'});
+// })
+
+app.get('/', helloWorld);
 
 app.listen(3333);
