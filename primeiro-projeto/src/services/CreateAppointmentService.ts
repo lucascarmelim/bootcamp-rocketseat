d527516a1c55d0050a89dabc56f36e8372e2ef2c
Lucas Carmelim <lucasdcarmelim@gmail.com>
2021-01-04 22:15:55 -0300

    Avanço das aulas

---
+++ primeiro-projeto/src/services/CreateAppointmentService.ts
@@ -0,0 +1,38 @@
+import Appointment from '../models/Appointment'
+import AppointmentsRepository from '../repository/AppointmentsRepository'
+import { startOfHour } from 'date-fns';
+
+/**
+ * [X]Recebimento das informações
+ * [/]Tratativa de erros/exceções
+ * [ ]Acesso ao repositório
+ */
+
+interface Request {
+  provider: string,
+  date: Date
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ date, provider}: Request): Appointment {
+    const appointmentDate = startOfHour(date);
+    
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(appointmentDate);
+  
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+  
+    const appointment = this.appointmentsRepository.create({provider, date: appointmentDate});
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
