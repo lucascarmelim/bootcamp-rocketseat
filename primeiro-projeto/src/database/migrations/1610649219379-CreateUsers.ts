adc2a820df3b5d80449b8909c714dc252181e6bb
lucascarmelim <lucasdcarmelim@gmail.com>
2021-01-14 17:51:06 -0300

    Continuação da aula

--- primeiro-projeto/src/database/migrations/1610649219379-CreateUsers.ts
+++ primeiro-projeto/src/database/migrations/1610649219379-CreateUsers.ts
@@ -1,28 +1,31 @@
-import {MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export default class CreateAppointments1610133739364 implements MigrationInterface {
+export default class CreateUsers1610649219379 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: 'appointments',
+                name: 'users',
                 columns: [
                     {
                         name: 'id',
                         type: 'uuid',
                         isPrimary: true,
                         generationStrategy: 'uuid',
-                        default: 'uuid_generate_v4()'
+                        default: 'uuid_generate_v4()',
                     },
                     {
-                        name: 'provider',
+                        name: 'name',
                         type: 'varchar',
-                        isNullable: false
                     },
                     {
-                        name: 'date',
-                        type: 'timestamp with time zone',
-                        isNullable: false
-                    },                    
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
+                    },
                     {
                         name: 'created_at',
                         type: 'timestamp',
@@ -39,7 +42,7 @@ export default class CreateAppointments1610133739364 implements MigrationInterfa
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('appointments')
+        await queryRunner.dropTable('users')
     }
 
 }
