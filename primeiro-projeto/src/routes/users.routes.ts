a8d0e0a3e4e70703f6a73ac8eabfc2d1c18f36a8
lucascarmelim <lucasdcarmelim@gmail.com>
2021-01-15 15:15:37 -0300

    Avanço das aulas

--- primeiro-projeto/src/routes/users.routes.ts
+++ primeiro-projeto/src/routes/users.routes.ts
@@ -15,9 +15,15 @@ usersRouter.post('/', async (request, response) => {
       password
     })
 
-    delete user.password;
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
 
-    return response.json(user);
+    return response.json(userWithoutPassword);
 
   } catch (err){
     return response.status(400).json({ error: err.message })
